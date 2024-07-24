a91ee42b520c02c0f432ee26f4cbfd3668ddc23f
ekkx <geniusnaru@gmail.com>
2024-07-24 18:38:05 +0900

    feat: update app module

--- packages/backend/src/app.service.ts
+++ packages/backend/src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './common/prisma/prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+	constructor(private readonly prisma: PrismaService) {}
+
+	async getMessage(): Promise<string> {
+		const messages = await this.prisma.message.findUnique({ where: { id: 1 } });
+		return messages.body;
+	}
 }
