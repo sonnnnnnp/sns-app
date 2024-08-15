a49d780788aaf81de32a3b8ff2ce88ffa7927d0a
ekkx <geniusnaru@gmail.com>
2024-08-15 16:49:46 +0900

    feat: install oapi-gen

--- go.mod
+++ go.mod
@@ -2,16 +2,28 @@ module github.com/sonnnnnnp/sns-app
 
 go 1.22.5
 
-require github.com/labstack/echo v3.3.10+incompatible
+require (
+	github.com/getkin/kin-openapi v0.127.0
+	github.com/labstack/echo/v4 v4.12.0
+)
 
 require (
+	github.com/go-openapi/jsonpointer v0.21.0 // indirect
+	github.com/go-openapi/swag v0.23.0 // indirect
+	github.com/invopop/yaml v0.3.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.5.5 // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/perimeterx/marshmallow v1.1.5 // indirect
+	github.com/ugorji/go/codec v1.2.11 // indirect
 	golang.org/x/sync v0.8.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 require (
--- internal/app/bootstrap.go
+++ internal/app/bootstrap.go
@@ -4,15 +4,28 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/labstack/echo"
+	"github.com/labstack/echo/v4"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+
+	"github.com/sonnnnnnp/sns-app/internal/pkg/oapi"
 )
 
 type User struct {
 	gorm.Model
 	Name  string
 	Email string
+}
+
+type UserController struct{}
+
+func (u UserController) GetUsers(ctx echo.Context) error {
+	return ctx.JSON(http.StatusOK, oapi.User{
+		Id:    1,
+		Name:  "admin",
+		Email: "user@example.com",
+	})
 }
 
 func Init() {
@@ -26,17 +39,21 @@ func Init() {
 
 	e := echo.New()
 
-	e.GET("/", func(c echo.Context) error {
-		db.Create(&User{
-			Name:  "gorm",
-			Email: "gorm@example.com",
-		})
+	uc := UserController{}
 
-		var user User
-		db.Last(&user)
+	oapi.RegisterHandlers(e, uc)
 
-		return c.String(http.StatusOK, fmt.Sprintf("User ID: %d", user.ID))
-	})
+	// e.GET("/", func(c echo.Context) error {
+	// 	db.Create(&User{
+	// 		Name:  "gorm",
+	// 		Email: "gorm@example.com",
+	// 	})
+
+	// 	var user User
+	// 	db.Last(&user)
+
+	// 	return c.String(http.StatusOK, fmt.Sprintf("User ID: %d", user.ID))
+	// })
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
