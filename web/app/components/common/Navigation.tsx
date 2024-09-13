bb1c1f7fc321e01b1748f4bf8e8edd8f495a4a55
sonnnnnnp <sonnnnnnp2@gmail.com>
2024-09-13 18:20:42 +0900

    Navigationの項目修正とホバー追加

--- web/app/components/common/Navigation.tsx
+++ web/app/components/common/Navigation.tsx
@@ -1,23 +1,35 @@
 'use client';
 
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function Navigation() {
+    const pathname = usePathname()
     const navItems = [
         { href: '/home', label: 'Home' },
-        { href: '/chat', label: 'Chat' },
         { href: '/notifications', label: 'Notifications' },
-        { href: '/profile', label: 'Profile'},
-        { href: '/action', label: 'Action'},
+        { href: '/settings', label: 'Settings'},
+        { href: '/live', label: 'Live'},
+        { href: '/messages', label: 'Messages' },
+        { href: '/calls', label: 'Calls'},
     ];
+
     return (
         <div className="w-52">
             <div className="fixed">
-                <nav className="text-lg">
+                <nav className="text-xl">
                     <ul>
-                    {navItems.map( item => (
-                        <li key={item.label} className="w-52 p-3">
-                            <Link href={item.href}>{item.label}</Link>
+                    {navItems.map(item => (
+                        <li
+                            key={item.label}
+                            className="w-52 p-3 hover:bg-slate-100 dark:hover:bg-neutral-900 rounded-xl"
+                        >
+                            <Link 
+                                href={item.href}
+                                className={`block ${pathname === item.href ? 'font-bold' : ''}`}
+                            >
+                                {item.label}
+                            </Link>
                         </li>
                     ))}                
                     </ul>
