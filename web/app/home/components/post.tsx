2955b89c4bfcf485bae0462930eee326480fa19f
ekkx <geniusnaru@gmail.com>
2024-09-14 22:52:28 +0900

    feat: use props for timeline posts

--- web/app/home/components/post.tsx
+++ web/app/home/components/post.tsx
@@ -4,33 +4,43 @@ import { AvatarImage } from "@radix-ui/react-avatar";
 import { Heart, MessageCircle, Repeat2, Share } from "lucide-react";
 import { PostHandler } from "./post-handler";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
-export function Post() {
+export function Post(props: {
+  username: string;
+  display_name: string;
+  avatar_image_url: string;
+  content: string;
+  created_at: number;
+}) {
   return (
     <Card className="flex flex-col items-start gap-2 text-left text-sm md:w-[580px]">
       <CardContent className="w-full grid gap-6 pt-5 pb-2">
         <div className="space-x-3 flex">
           <div>
-            <Avatar>
-              <AvatarImage src="/kaworu_icon.jpg" />
-            </Avatar>
+            <Link href={`/users/${props.username}`}>
+              <Avatar>
+                <AvatarImage src={props.avatar_image_url} />
+              </Avatar>
+            </Link>
           </div>
           <div>
             <div className="flex items-center">
               <div className="flex items-center gap-1">
-                <p className="text-sm font-semibold leading-none">カヲル</p>
-                <p className="text-sm text-muted-foreground">@kaworu</p>
+                <span className="text-sm font-semibold leading-none">
+                  {props.display_name}
+                </span>
+                <span className="text-sm text-muted-foreground">
+                  {`@${props.username}`}
+                </span>
+                <span className="text-xs text-muted-foreground">{` ･ 3分前`}</span>
               </div>
               <div className="ml-auto">
                 <PostHandler />
               </div>
             </div>
             <div className="mt-1 mb-2">
-              <p className="text-xs text-muted-foreground">
-                {
-                  "朝目覚まし止めめたつの罠にしまった朝目覚まし止めめたつの罠にしまった朝目覚まし止めめたつの罠にしまった朝目覚まし止めめたつの罠にしまった"
-                }
-              </p>
+              <p className="text-xs text-muted-foreground">{props.content}</p>
             </div>
             <div className="flex items-center justify-between opacity-60">
               <Button variant={"ghost"} size={"sm"}>
--- web/app/home/components/timeline.tsx
+++ web/app/home/components/timeline.tsx
@@ -7,15 +7,48 @@ export function Timeline() {
     <div className="mx-3">
       <TimelineTab />
       <div className="grid gap-y-3 pt-[68px]">
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+        <Post
+          username="kaworu"
+          display_name="カヲル"
+          avatar_image_url="/kaworu_icon.jpg"
+          content="ユーザーがアクセスできるかは別として貴重なデータとして保存はしておきたい"
+          created_at={1234567}
+        />
+        <Post
+          username="kaworu"
+          display_name="カヲル"
+          avatar_image_url="/kaworu_icon.jpg"
+          content="ユーザーがアクセスできるかは別として貴重なデータとして保存はしておきたい"
+          created_at={1234567}
+        />
+        <Post
+          username="kaworu"
+          display_name="カヲル"
+          avatar_image_url="/kaworu_icon.jpg"
+          content="ユーザーがアクセスできるかは別として貴重なデータとして保存はしておきたい"
+          created_at={1234567}
+        />
+        <Post
+          username="kaworu"
+          display_name="カヲル"
+          avatar_image_url="/kaworu_icon.jpg"
+          content="ユーザーがアクセスできるかは別として貴重なデータとして保存はしておきたい"
+          created_at={1234567}
+        />
+        <Post
+          username="kaworu"
+          display_name="カヲル"
+          avatar_image_url="/kaworu_icon.jpg"
+          content="ユーザーがアクセスできるかは別として貴重なデータとして保存はしておきたい"
+          created_at={1234567}
+        />
+        <Post
+          username="kaworu"
+          display_name="カヲル"
+          avatar_image_url="/kaworu_icon.jpg"
+          content="ユーザーがアクセスできるかは別として貴重なデータとして保存はしておきたい"
+          created_at={1234567}
+        />
       </div>
     </div>
   );
