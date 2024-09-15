18eb133f0e4dbdd9cf88f87b8edde3fb8e1f6daf
sonnnnnnp <sonnnnnnp2@gmail.com>
2024-09-16 02:22:14 +0900

    Add ScrollArea(shadcn/ui)

--- web/app/messages/ChatList.tsx
+++ web/app/messages/ChatList.tsx
@@ -6,19 +6,20 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 const ChatList = () => {
   return (
     <div className="flex flex-col h-full">
       <CardTitle className="m-4 text-xl">メッセージ</CardTitle>
       <Separator />
-      <div className="flex-1 overflow-y-auto">
-      {[...Array(30)].map((_, index) => (
-        <ChatListItem key={index} />
-      ))}
-      </div>
+      <ScrollArea className="flex-1 overflow-y-auto">
+        {[...Array(30)].map((_, index) => (
+          <ChatListItem key={index} />
+        ))}
+      </ScrollArea>
     </div>
   );
 };
