6dcf53ba1ad20f60c265206a4cc804d4dfa69b9a
sonnnnnnp <sonnnnnnp2@gmail.com>
2024-09-15 23:23:23 +0900

    Add header to chat list

--- web/app/messages/ChatList.tsx
+++ web/app/messages/ChatList.tsx
@@ -1,11 +1,24 @@
 import ChatListItem from "./ChatListItem";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Separator } from "@/components/ui/separator";
 
 const ChatList = () => {
   return (
-    <div className="overflow-y-scroll">
+    <div className="flex flex-col h-full">
+      <CardTitle className="m-4 text-xl">メッセージ</CardTitle>
+      <Separator />
+      <div className="flex-1 overflow-y-auto">
       {[...Array(30)].map((_, index) => (
         <ChatListItem key={index} />
       ))}
+      </div>
     </div>
   );
 };
