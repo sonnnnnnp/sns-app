a472b96f4a290e24b2f1c79d9e9aa4a3b26c3571
sonnnnnnp <sonnnnnnp2@gmail.com>
2024-09-16 12:05:03 +0900

    ChatBubbleコンポーネント追加

--- web/app/messages/ChatWindow.tsx
+++ web/app/messages/ChatWindow.tsx
@@ -1,11 +1,33 @@
 import { Separator } from "@/components/ui/separator";
+import { ChatMessageList } from "@/components/ui/chat/chat-message-list";
+import {
+  ChatBubble,
+  ChatBubbleAvatar,
+  ChatBubbleMessage,
+} from "@/components/ui/chat/chat-bubble";
 
 const ChatWindow = () => {
   return (
-    <div className="hidden md:block">
-      <Separator orientation="vertical" />
-      <div className="mx-40">
-        <Separator />
+    <div className="hidden md:flex h-full">
+      <Separator orientation="vertical" className="h-full" />
+      <div className="flex-1 overflow-hidden max-w-[480px]">
+        <ChatMessageList>
+          {[...Array(30)].map((_, i) => (
+            <ChatBubble>
+              <ChatBubbleAvatar />
+              <ChatBubbleMessage>
+                Message and other content here
+              </ChatBubbleMessage>
+            </ChatBubble>
+          ))}
+          <ChatBubble className="max-w-[80%]">
+            <ChatBubbleAvatar />
+            <ChatBubbleMessage>
+              この問題を解決するには、フレックスボックスレイアウトを使用して水平方向にコンポーネントを配置する必要があります。以下のように
+              ChatWindow コンポーネントを修正することをお勧めします：
+            </ChatBubbleMessage>
+          </ChatBubble>
+        </ChatMessageList>
       </div>
     </div>
   );
