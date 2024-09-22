5c846ecc95fc9959baed84b192de7262961d27cb
sonnnnnnp <sonnnnnnp2@gmail.com>
2024-09-22 18:28:23 +0900

    Navigation.tsx Lucideアイコン追加

--- web/components/common/Navigation.tsx
+++ web/components/common/Navigation.tsx
@@ -1,19 +1,28 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import {
+  Home,
+  Search,
+  Phone,
+  Video,
+  MessageSquare,
+  Bell,
+  Settings,
+} from "lucide-react";
 
 export default function Navigation() {
   const pathname = usePathname();
   const navItems = [
-    { href: "/home", label: "ホーム" },
-    { href: "/search", label: "検索" },
-    { href: "/calls", label: "通話" },
-    { href: "/live", label: "ライブ" },
-    { href: "/messages", label: "メッセージ" },
-    { href: "/notifications", label: "通知" },
-    { href: "/settings", label: "設定" },
+    { href: "/home", label: "ホーム", icon: Home },
+    { href: "/search", label: "検索", icon: Search },
+    { href: "/calls", label: "通話", icon: Phone },
+    { href: "/live", label: "ライブ", icon: Video },
+    { href: "/messages", label: "メッセージ", icon: MessageSquare },
+    { href: "/notifications", label: "通知", icon: Bell },
+    { href: "/settings", label: "設定", icon: Settings },
   ];
-  
+
   return (
     <div className="w-52 hidden md:block">
       <div className="fixed">
@@ -22,15 +31,23 @@ export default function Navigation() {
             {navItems.map((item) => (
               <li
                 key={item.label}
-                className="w-52 p-3 hover:bg-slate-100 dark:hover:bg-neutral-900 rounded-xl"
+                className={`w-52 p-3  rounded-xl ${
+                  pathname === item.href
+                    ? "bg-slate-100 dark:bg-neutral-900 hover:bg-slate-200"
+                    : "hover:bg-slate-100 dark:hover:bg-neutral-900"
+                }`}
               >
                 <Link
                   href={item.href}
-                  className={`block ${
+                  className={`flex items-center ${
                     pathname === item.href ? "font-bold" : ""
                   }`}
                 >
-                  {item.label}
+                  <item.icon
+                    size={24}
+                    strokeWidth={pathname === item.href ? "2.5" : "2"}
+                  />
+                  <span className="ml-4">{item.label}</span>
                 </Link>
               </li>
             ))}
