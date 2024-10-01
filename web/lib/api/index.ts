014c8012625998e8a6229d76a951b173e2d1c064
ekkx <geniusnaru@gmail.com>
2024-10-01 11:41:14 +0900

    fix: setting cookie tokens after refresing

--- web/lib/api/index.ts
+++ web/lib/api/index.ts
@@ -36,7 +36,7 @@ const requestInterceptor: Middleware = {
       const refreshResponse = await refreshAuthorization(refreshToken);
       if (refreshResponse.ok) {
         Cookie.setAccessToken(refreshResponse.access_token!);
-        Cookie.setAccessToken(refreshResponse.refresh_token!);
+        Cookie.setRefreshToken(refreshResponse.refresh_token!);
         return undefined;
       }
 
