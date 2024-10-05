201bbca22ad035a255ce86c5ba8063657a86e0ce
ekkx <geniusnaru@gmail.com>
2024-10-06 00:33:49 +0900

    feat: タイムラインのローディングアニメーションを追加

--- web/app/home/components/post-list.tsx
+++ web/app/home/components/post-list.tsx
@@ -1,3 +1,4 @@
+import { Skeleton } from "@/components/ui/skeleton";
 import { Post } from "./post";
 import { components } from "@/lib/api/client";
 
@@ -8,16 +9,34 @@ type Props = {
 export function PostList({ posts }: Props) {
   return (
     <div>
-      {posts.map((post, i) => (
-        <Post
-          key={i}
-          username={post.author.username}
-          display_name={post.author.display_name}
-          avatar_image_url={post.author.avatar_url}
-          content={post.content ?? ""}
-          created_at={post.created_at}
-        />
-      ))}
+      {posts.length === 0
+        ? Array.from({ length: 20 }).map((_, i) => (
+            <div key={i} className="flex border-b pl-4 pr-3 pt-4 pb-6">
+              <div className="mt-2 mr-2">
+                <Skeleton className="h-10 w-10 rounded-full" />
+              </div>
+              <div className="flex-1 grid gap-y-2">
+                <div className="flex items-center"></div>
+                <div className="grid gap-y-1 space-y-4">
+                  <div className="space-y-2">
+                    <Skeleton className="h-4 w-[80%]" />
+                    <Skeleton className="h-4 w-[30%]" />
+                  </div>
+                  <Skeleton className="h-4 w-[80%]" />
+                </div>
+              </div>
+            </div>
+          ))
+        : posts.map((post, i) => (
+            <Post
+              key={i}
+              username={post.author.username}
+              display_name={post.author.display_name}
+              avatar_image_url={post.author.avatar_url}
+              content={post.content ?? ""}
+              created_at={post.created_at}
+            />
+          ))}
     </div>
   );
 }
